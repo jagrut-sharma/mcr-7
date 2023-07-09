aa16e2a44eabc1327f4fe9e802a290f920fdda89
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-07-09 21:55:39 +0530

    completed project

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -17,27 +17,52 @@ export default function Details() {
     ({ id }) => id === +destinationID
   );
 
-  console.log(selectedDestination);
-
   return (
     <main className="flex flex-col items-center">
       <h1 className="text-center p-4 pt-8 text-3xl text-white font-bold">
         {selectedDestination.name}
       </h1>
 
-      <div className="flex items-center justify-center mt-4 w-[60%] gap-4">
-        <div>
+      <div className="flex justify-center mt-4 w-[60%] gap-4">
+        <div className="h-max">
           <img
             src={selectedDestination.image}
             alt={selectedDestination.name}
-            className="w-[20rem]"
+            className=""
           />
         </div>
-        <div>
+        <div className="flex flex-col gap-4">
           <h1 className="text-white">
             <span className="text-violet-500">Description: </span>
             {selectedDestination.description}
           </h1>
+
+          <p className="text-white">
+            {" "}
+            <span className="text-violet-500">Ratings: </span>
+            {selectedDestination.ratings}
+          </p>
+          <p className="text-white">
+            {" "}
+            <span className="text-violet-500">Reviews: </span>
+            {selectedDestination.reviews}
+          </p>
+          <p className="text-white">
+            {" "}
+            <span className="text-violet-500">Location: </span>
+            {selectedDestination.location}
+          </p>
+          <p className="text-white">
+            {" "}
+            <span className="text-violet-500">Opening Hours: </span>
+            {selectedDestination.openingHours}
+          </p>
+          <p className="text-white">
+            {" "}
+            <span className="text-violet-500">Ticket price : </span>
+            {selectedDestination.ticketPrice}
+          </p>
+          <p className="text-red-300">Website</p>
         </div>
       </div>
     </main>
